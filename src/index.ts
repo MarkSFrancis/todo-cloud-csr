291886904c0732231bd1be0235456208bd4520b5
Mark Francis <mrkfrancis8@gmail.com>
2024-02-04 13:37:20 +0000

    Added oauth (requires JWT_SECRET)

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 import { Hono } from 'hono';
 import { db } from './db/client';
+import { jwt } from 'hono/jwt';
 
 export type Env = {
 	DB: D1Database;
+	JWT_SECRET: string;
 };
 
 const app = new Hono<{ Bindings: Env }>();
+
+app.use('/api/*', (c, next) => {
+	const auth = jwt({
+		secret: c.env.JWT_SECRET,
+	});
+
+	return auth(c, next);
+});
 
 app.get('/api/users', async (c) => {
 	const results = await db(c.env.DB).query.users.findMany();
